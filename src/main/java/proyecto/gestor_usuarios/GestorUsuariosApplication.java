ae028ea10ecbb63bba3cb849ef765ceaaff1cbd6
dinhodavid11 <dinhodavid11@gmail.com>
2023-07-31 16:57:45 -0500

    Hola mundo

---
+++ src/main/java/proyecto/gestor_usuarios/GestorUsuariosApplication.java
@@ -0,0 +1,13 @@
+package proyecto.gestor_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proyecto/gestor_usuarios/GestorUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.gestor_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
